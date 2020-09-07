120cb6b30aa94b8cc8ddd4b092017c4963030ace
jjjzy <yujiazhenn@hotmail.com>
2020-09-07 21:41:32 +0000

    first commit jiazhen Yu

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,34 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+	sum = arr.inject(0, :+)
+    return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	max_arr = arr.max(2)
+	return sum(max_arr)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+	
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+	puts "Hello World!"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+	puts "Hello World!"
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+	puts "Hello World!"
 end
 
 # Part 3
